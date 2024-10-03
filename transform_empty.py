004ea51a00bb6de92d56b82fe7a9242b3f2a6ac5
Suubi08 <suubikaneb4@gmail.com>
2024-10-03 15:39:37 +0300

    first commit

---
+++ transform_empty.py
@@ -0,0 +1,22 @@
+from image import Image
+import numpy as np
+
+def brighten(image, factor):
+    pass
+
+def adjust_contrast(image, factor, mid):
+    pass
+
+def blur(image, kernel_size):
+    pass
+
+def apply_kernel(image, kernel):
+    pass
+
+def combine_images(image1, image2):
+    pass
+    
+if __name__ == '__main__':
+    lake = Image(filename='lake.png')
+    city = Image(filename='city.png')
+
